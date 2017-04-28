e0d8fbc770b3a28085f06ef7e5700615b417a886
ElizabethMadrid <elymadrid009@hotmail.com>
2017-04-27 20:07:36 -0500

    modificación de vista

--- app/src/main/java/com/practica1/elii/drapp/Receta.java
+++ app/src/main/java/com/practica1/elii/drapp/Receta.java
@@ -1,7 +1,10 @@
 package com.practica1.elii.drapp;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 /**
  * Created by Elii on 03/04/2017.
@@ -9,11 +12,24 @@ import android.support.v7.app.AppCompatActivity;
 
 public class Receta extends AppCompatActivity {
 
+    /*RecyclerView recyclerView;
+    RecyclerView.LayoutManager layoutManager;
+    Receta1 adapter;*/
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.receta);
 
+        /*recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        adapter = new Receta1();
+        recyclerView.setAdapter(adapter);*/
     }
+
+
+
 }
 
