62033c484d7ef11e16f43ca563b8007be7bce137
titis0892 <titiska_mata@hotmail.com>
2017-04-29 19:00:19 -0500

    Cambio de onClik en la vista de analisis

--- app/src/main/java/com/practica1/elii/drapp/analisis.java
+++ app/src/main/java/com/practica1/elii/drapp/analisis.java
@@ -25,10 +25,14 @@ public class analisis extends AppCompatActivity {
                 .setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        Aceptar();
+                        AceptarGlucosa();
 
                     }
                 }).create().show();
+    }
+    public void AceptarGlucosa() {
+
+
 
     }
 
@@ -44,13 +48,22 @@ public class analisis extends AppCompatActivity {
                 .setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        Aceptar();
+                        AceptarColesterol();
 
                     }
                 }).create().show();
-
-
     }
+    public void AceptarColesterol() {
+   }
+
+
+
+
+
+
+
+
+
     public void hemoglobina (View view){
         new AlertDialog.Builder(this)
                 .setTitle("HEMOGLOBINA")
@@ -58,17 +71,19 @@ public class analisis extends AppCompatActivity {
                         " oxígeno a los órganos de su cuerpo y los tejidos y transporta el dióxido de carbono de " +
                         "los órganos y tejidos de nuevo a los pulmones.")
 
-
                 .setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        Aceptar();
+                        AceptarHemoglobina();
 
                     }
                 }).create().show();
-
-
     }
+    public void AceptarHemoglobina() {
+    }
+
+
+
     public void leucocitos (View view) {
         new AlertDialog.Builder(this)
                 .setTitle("LEUCOCITOS")
@@ -76,18 +91,17 @@ public class analisis extends AppCompatActivity {
                         " son un componente importante de la sangre y una pieza clave en" +
                         " el sistema inmunológico del cuerpo.")
 
-
                 .setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        Aceptar();
+                        AceptarLeucocitos();
 
                     }
                 }).create().show();
-
-
-
     }
+    public void AceptarLeucocitos() {
+    }
+
     public void eritrocitos (View view){
         new AlertDialog.Builder(this)
                 .setTitle("ERITROCITOS")
@@ -99,13 +113,16 @@ public class analisis extends AppCompatActivity {
                 .setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        Aceptar();
+                        AceptarEritrocitos();
 
                     }
                 }).create().show();
-
-
     }
+    public void AceptarEritrocitos() {
+    }
+
+
+
     public void urea (View view){
         new AlertDialog.Builder(this)
                 .setTitle("UREA")
@@ -117,17 +134,123 @@ public class analisis extends AppCompatActivity {
                 .setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        Aceptar();
+                        AceptarUrea();
 
                     }
                 }).create().show();
-
-
+    }
+    public void AceptarUrea() {
     }
 
 
 
+    public void calcio (View view){
+        new AlertDialog.Builder(this)
+                .setTitle("CALCIO")
+                .setMessage("El calcio es un macromineral que cumple una importante " +
+                        "función estructural en nuestro organismo al ser parte integrante de huesos y dientes." +
+                        "Sin embargo, para la fijación del calcio en el sistema óseo es necesaria " +
+                        "la presencia de Vitamina D.")
 
+
+                .setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        AceptarCalcio();
+
+                    }
+                }).create().show();
+    }
+    public void AceptarCalcio() {
+
+
+
+    }
+    public void trigliceridos (View view){
+        new AlertDialog.Builder(this)
+                .setTitle("TRIGLICERIDOS")
+                .setMessage("Los triglicéridos son un tipo de grasa que se encuentra en la sangre." +
+                        " El exceso de este tipo de grasa puede aumentar el riesgo de enfermedad de las arterias coronarias")
+
+
+
+                .setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        AceptarTrigliceridos();
+
+                    }
+                }).create().show();
+    }
+    public void AceptarTrigliceridos() {
+
+
+
+    }
+    public void bilirrubina (View view){
+        new AlertDialog.Builder(this)
+                .setTitle("BILIRRUBINAS")
+                .setMessage("La bilirrubina es un pigmento, de color amarillo anaranjado, presente en la sangre." +
+                        "Es uno de los parámetros que se analizan en un análisis de sangre y ayuda a saber cómo " +
+                        "funciona el hígado.")
+
+
+                .setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        AceptarBilirrubina();
+
+                    }
+                }).create().show();
+    }
+    public void AceptarBilirrubina() {
+
+
+
+    }
+    public void ph (View view){
+        new AlertDialog.Builder(this)
+                .setTitle("PH")
+                .setMessage("El pH es una forma de representar el carácter ácido, neutro o básico de una solución acuosa." +
+                        " El agua pura está levemente disociada en iones H+ e iones OH- por partes iguales." +
+                        "Cuando la cantidad de iones H+ es mayor a la de iones OH- la solución es ácida." +
+                        "En caso inverso la solución es básica..")
+
+
+                .setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        AceptarPh();
+
+                    }
+                }).create().show();
+    }
+    public void AceptarPh() {
+
+
+
+    }
+    public void bacterias(View view){
+        new AlertDialog.Builder(this)
+                .setTitle("BACTERIAS")
+                .setMessage("Las bacterias son microorganismos unicelulares, con movilidad propia y que ostentan un muy pequeño " +
+                        "tamaño y diversidad en su forma: esferas, barras, hélices, entre otras.")
+
+
+
+                .setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        AceptarBacterias();
+
+                    }
+                }).create().show();
+    }
+    public void AceptarBacterias() {
+
+
+
+    }
 
     public void Aceptar(){
         Intent explicit_intent;
